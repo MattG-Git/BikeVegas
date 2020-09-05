6ce4dde0f4ed39041b2a4bb96eae20976c5c6339
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-09-05 00:27:48 -0700

    Sixth commit - Bike shop

---
+++ JS/bike.js
@@ -0,0 +1,25 @@
+//Global Filter Variables
+const priceSelector1 = document.getElementById('cost_1');
+const priceSelector2 = document.getElementById('cost_2');
+const priceSelector3 = document.getElementById('cost_3');
+
+const rentalY = document.getElementById('rentals-Y');
+const rentalN = document.getElementById('rentals-N');
+
+const tourY = document.getElementById('tours-Y');
+const tourN = document.getElementById('tours-N');
+
+//Bike Shop Card Variables
+const mcghies = document.getElementById('mcghies');
+const riverMountainBike = document.getElementById('rivermountainbike');
+const pedego = document.getElementById('pedego');
+const rei = document.getElementById('rei');
+const bicycleLab = document.getElementById('bicyclelab');
+const allMountainCyclery = document.getElementById('allmountaincyclery');
+const dicks = document.getElementById('dicks');
+const big5 = document.getElementById('big5');
+const giantLv = document.getElementById('giantlv');
+const velofix = document.getElementById('velofix');
+const cycleGear = document.getElementById('cyclegear');
+const proShop = document.getElementById('proshop');
+
